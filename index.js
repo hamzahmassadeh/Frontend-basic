d10cd3b23f36aff65f45987db0d5da9cf255a2e0
hamzahmassadeh <masadeh602@gmail.com>
2024-03-30 12:27:11 +0300

    more progress

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+function fav(num){
+
+    var colors = document.getElementsByClassName('fa-heart')[num];
+    if(colors.style.color=='white'){
+        colors.style.color='red'; 
+    }
+    else{
+        colors.style.color='white'; 
+    }
+
+       
+
+
+
+
+
+
+    
+  
+
+}
